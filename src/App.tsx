bbf7bcca0da1ec5078294acc7c7c57666833455a
Natalia Braz <naabraz@gmail.com>
2023-05-23 09:54:06 -0300

    chore: move event delete to recoil

--- src/App.tsx
+++ src/App.tsx
@@ -42,9 +42,9 @@ function App() {
     // }
     // setEventos([...eventos]);
   };
-  const deletarEvento = (id: number) => {
-    // setEventos([...eventos.filter((evento) => evento.id !== id)]);
-  };
+  // const deletarEvento = (id: number) => {
+  //   // setEventos([...eventos.filter((evento) => evento.id !== id)]);
+  // };
 
   const aplicarFiltro = (data: Date | null) => {
     setFiltro(data);
@@ -70,7 +70,6 @@ function App() {
             <ListaDeEventos
               aoFiltroAplicado={aplicarFiltro}
               aoAlterarStatus={alterarStatusEvento}
-              aoDeletarEvento={deletarEvento}
             />
           </Card>
         </div>
--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -8,9 +8,8 @@ import { listaDeEventosState } from '../../state/atom';
 
 const ListaDeEventos: React.FC<{
   aoAlterarStatus: (id: number) => void;
-  aoDeletarEvento: (id: number) => void;
   aoFiltroAplicado: (data: Date | null) => void;
-}> = ({ aoDeletarEvento, aoAlterarStatus, aoFiltroAplicado }) => {
+}> = ({ aoAlterarStatus, aoFiltroAplicado }) => {
   const eventos = useRecoilValue(listaDeEventosState);
 
   return (
@@ -20,7 +19,6 @@ const ListaDeEventos: React.FC<{
         {eventos.map((evento) => (
           <Evento
             aoAlterarStatus={aoAlterarStatus}
-            aoDeletarEvento={aoDeletarEvento}
             evento={evento}
             key={evento.id}
           />
